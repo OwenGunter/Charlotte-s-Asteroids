4cf705b306bac43276b1c010a013cee9c9a1aea1
OwenGunter <owen.gunter@student.temple.sa.edu.au>
2016-05-04 20:56:40 +0930

    Charlotte's Asteroids
    All the collisions are working as they should except for when the player
    collides with the enemy and spins out of control. Also the player dies
    after 2 shots from the enemy.

---
+++ the actual one/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public int EnemyShipSpeed;
+
+    // A reference to the player - this will be set when the enemy is loaded
+    private GameObject m_Player;
+
+    // A reference to the nav mesh agent component
+    private NavMeshAgent m_NavAgent;
+
+    // A reference to the rigidbody component
+    private Rigidbody m_Rigidbody;
+
+    private void Awake ()
+    {
+        m_Player = GameObject.FindGameObjectWithTag ("Player");
+        m_NavAgent = GetComponent<NavMeshAgent> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+    }
+
+    private void OnEnable ()
+    {
+        // when the ship is turned on, make sure it is not kinematic
+        m_Rigidbody.isKinematic = false;
+    }
+}
---
+++ the actual one/Assets/Scripts/GameManager/EnemyHearts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHearts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
